26c33718193d2df15b39c0fd444c85dd20da31af
Skorodinskaya <skorodinskaya01@gmail.com>
2021-10-05 20:10:56 +0300

    init commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,31 @@
+// const {read} = require('../helper/user_helper');
+
+module.exports = {
+    getUsers: (req, res) => {
+        // read().then(users => {
+        //     res.json(users)
+        // })
+    },
+    //
+    // getUserById: (req, res) => {
+    //     const {user_id} = req.params;
+    //
+    //     read().then(users => {
+    //         res.json(users[user_id - 1])
+    //     })
+    // },
+    //
+    // createUser: (req, res) => {
+    //     read().then(users => {
+    //         res.json(users);
+    //         // write(mkdirUsers, JSON.stringify(users));
+    //     })
+    // },
+    //
+    // deleteUserById: (req, res) => {
+    //     read().then(users => {
+    //         res.json(users);
+    //         // delete();
+    //     })
+    // }
+}
