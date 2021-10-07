bdd0fd21761492528155a36eeae6993300bdacca
Skorodinskaya <skorodinskaya01@gmail.com>
2021-10-07 21:31:52 +0300

    some changes

---
+++ middlewares/user.middleware.js
@@ -0,0 +1,16 @@
+const User = require('../dataBase/User');
+
+module.exports = {
+    createUserMiddleware: async (req, res, next) => {
+        try {
+            const userByEmail = await User.findOne({email: req.body.email});
+
+            if (userByEmail) {
+                throw new Error('Email already exists');
+            }
+            next();
+        } catch (e) {
+            res.json(e.message);
+        }
+    }
+};
