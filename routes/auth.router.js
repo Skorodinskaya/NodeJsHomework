e50f08a74dc618245a4a27b5142df70e9b9a076a
Skorodinskaya <skorodinskaya01@gmail.com>
2021-10-15 01:08:28 +0300

    some changes

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,12 +1,11 @@
 const authRouter = require('express').Router();
 
 const {authController} = require('../controllers');
-const {authMiddleware, userMiddleware} = require('../middlewares');
+const {authMiddleware} = require('../middlewares');
 
 authRouter.post('/login',
     authMiddleware.isAuthValid,
     authMiddleware.authLoginMiddleware,
-    userMiddleware.isUserPresent,
     authController.loginController);
 
 module.exports = authRouter;
